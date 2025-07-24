5e8bf607cc6caabf5ca0aff96bd0befd9a7c086c
satish-orotron <satish.h@orotron.com>
2025-07-24 07:44:49 +0530

    SG update the Task2

---
+++ src/components/content/index.tsx
@@ -0,0 +1,12 @@
+import Header from "../header";
+
+const Content = () => {
+    return (
+        <div>
+            <div>
+                <Header />
+            </div>
+        </div>
+    )
+}
+export default Content;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,8 @@
-import Header from '@/components/header'
+import Content from '@/components/content'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
-  component: Header,
+  component: Content,
 
 })
 
